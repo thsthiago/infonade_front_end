75c7ed46d29a31b050e74103ae964bdf076c40cb
Thiago Cabral <thiagocabral477@gmail.com>
2021-09-10 21:30:59 -0300

    Adding dependencs

---
+++ src/styles/global.ts
@@ -0,0 +1,69 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyles = createGlobalStyle`
+  :root {
+    --primary: #25232F;
+    --secondary: #413D51;
+    --background: #F1F8F4;
+    --border-color: #413D51;
+    --blue-200: #8FE3F9;
+    --blue-100: #B2EBF9;
+    --text: #F2F2F2;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: none;
+  }
+
+  html,
+  body {
+    width: 100%;
+    height: 100%;
+  }
+
+  #__next  {
+    width: 100%;
+    height: 100%;
+  }
+
+  h1, h2, h3, h4, h5 {
+    font-family: 'Oswald', sans-serif;
+  }
+
+  p,
+  a,
+  strong,
+  span,
+  li,
+  tr,
+  td {
+    font-family: 'Roboto', sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  body {
+    background: var(--background);
+  }
+
+  button {
+    outline: none;
+    cursor: pointer;
+    border: none;
+  }
+
+  &::-webkit-scrollbar {
+    width: 8px;
+    background: transparent;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: var(--primary);
+    border-radius: 8px;
+  }
+`
